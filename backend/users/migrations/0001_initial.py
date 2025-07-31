946de2ebb5998476a2ad1f1763bf2f2f4cf42088
Salah Moustafa <153738101+salahamran@users.noreply.github.com>
2025-07-31 22:14:04 +0300

    fixing flake8 v3

--- backend/users/migrations/0001_initial.py
+++ backend/users/migrations/0001_initial.py
@@ -4,6 +4,7 @@ import django.contrib.auth.models
 import django.core.validators
 import django.db.models.deletion
 import django.utils.timezone
+
 from django.conf import settings
 from django.db import migrations, models
 
