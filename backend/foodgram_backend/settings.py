f90d38301c946f6b436d7a3287c84bc6b0578d56
Salah Moustafa <153738101+salahamran@users.noreply.github.com>
2025-08-06 01:39:02 +0300

    fix csrf error

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -129,7 +129,7 @@ DJOSER = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
