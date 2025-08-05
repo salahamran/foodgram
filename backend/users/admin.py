f23af5ee2de8b16311d592f578a86cb920b3bc60
Salah Moustafa <153738101+salahamran@users.noreply.github.com>
2025-08-06 01:03:35 +0300

    update for csrf and auto serilaziers

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from users.models import Subscription, User
+from users.models import Subscription
+from users.models import User
 
 
 @admin.register(User)
